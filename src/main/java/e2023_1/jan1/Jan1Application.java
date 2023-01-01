7dadba2748ca45881d4c33a2ddbaa492305d42eb
OrangeDragon1 <wcks095@gmail.com>
2023-01-01 17:03:04 +0800

    initial commit

---
+++ src/main/java/e2023_1/jan1/Jan1Application.java
@@ -0,0 +1,13 @@
+package e2023_1.jan1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jan1Application.class, args);
+	}
+
+}
---
+++ src/test/java/e2023_1/jan1/Jan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package e2023_1.jan1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
